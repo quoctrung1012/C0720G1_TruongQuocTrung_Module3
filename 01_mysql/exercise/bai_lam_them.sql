d5dafc21a1f0e7f9e81fe527615b813b9f8bba57
unknown <quoctrung1012@gmail.comgit config --global user.name quoctrung1012>
2020-10-20 08:01:39 +0700

    guibai

---
+++ 01_mysql/exercise/bai_lam_them.sql
@@ -0,0 +1,23 @@
+create database bai_tap_lam_them;
+use bai_tap_lam_them;
+create table Products(
+Id int,
+Name nvarchar(50),
+UmageUrl nvarchar(1000),
+Price int,
+Discount int,
+Stock int,
+CategoryId int,
+SupplierId int,
+Description nvarchar(1000));
+create table Orders(
+Id int,
+CreatedDate datetime,
+ShippedDate datetime,
+Status varchar(50),
+Description nvarchar(1000),
+ShippingAddress nvarchar(500),
+ShippingCity nvarchar(50),
+PaymentType varchar(20),
+Customer varchar(20),
+Employee varchar(20));
---
+++ 01_mysql/practice/01_create_and_drop_database.sql
@@ -0,0 +1,2 @@
+ -- CREATE DATABASE `my_database`;
+ DROP DATABASE `my_database`;
